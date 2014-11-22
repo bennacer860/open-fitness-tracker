e7f65712848f249508b49ad1fbbc85bfa434dbda
bennacer860 <bennacer860@gmail.com>
2014-11-21 23:20:27 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenFitnessTracker::Application.config.secret_token = '5d433c5607a9dc3067d610ee9f0372d33bc66b1f7612ebabd1cb74b3f6c4126d8963c1a737139fdeb6f548faf8b0193804056d84514303e8d40ce7e0c2d09a18'
