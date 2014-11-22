f536b73fd5b6b2cccb91e6b9e6368cb7e192f4b2
bennacer860 <bennacer860@gmail.com>
2014-11-22 00:03:10 -0500

    add callback route for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 OpenFitnessTracker::Application.routes.draw do
 
-  get "session/create"
+  match "/auth/:provider/callback", to: "session#create" , via: [:get,:post]
+  match "/logout", to: "session#destroy" , via: [:get,:post]
 
   get "session/destroy"
 
