c590d2fafc8ed8262755e9eda3b113fb681b8a6b
bennacer860 <bennacer860@gmail.com>
2014-11-22 19:47:51 -0500

    fix fitbit strategy from DB and change consumer key/secret. If changed again, they would need to be entered in the DB

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :fitbit,  Configuration.where(:name=>"consumer_key").first.value, Configuration.where(:name=>"consumer_secret").first.value
+
+  provider :fitbit, Configuration.where(:name=>"consumer_key").first.value, Configuration.where(:name=>"consumer_secret").first.value
+
 end
 
